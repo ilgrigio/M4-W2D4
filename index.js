b7c86662ec72ef1e58e11b02506c844da9bfac5e
Andrea Sattin <79341965+ilgrigio@users.noreply.github.com>
2024-01-18 18:27:15 +0100

    Piccole modifiche CSS

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const container = document.querySelector(".container");
 fetch(apiBooks, {
   method: "GET",
 })
-  .then((response) => {
-    return response.json();
-  }) // Dopo aver ricevuto la response ciclo con .map i libri inserendoli nell'HTML
+  .then((response) => response.json()) // Dopo aver ricevuto la response ciclo con .map i libri inserendoli nell'HTML
   .then((dataBooks) => {
     dataBooks.map((book) => {
       container.innerHTML += `
@@ -32,6 +30,7 @@ fetch(apiBooks, {
     </div>
   </div>`;
     });
+    // Richiamo gli EventListener
     const hideButton = document.querySelectorAll("#hide");
     hideButton.forEach((hide) => {
       hide.addEventListener("click", hideCard);
@@ -44,7 +43,8 @@ fetch(apiBooks, {
   .catch((err) => {
     console.error(err);
   });
-// Creo una funzione che mi permette di aggiungere e togliere la selezione alle card
+
+// Creo una funzione che mi permette di aggiungere la selezione alle card
 // tramite delle classi CSS
 const select = (clickEvent) => {
   const selected = document.querySelectorAll(".selected");
@@ -59,67 +59,4 @@ const hideCard = (hideEvent) => {
   hideEvent.target.closest(".card").classList.add("hide");
 };
 
-// SearchBar
-// Funzione event per poter ricercare nella Searchbar
-//! Inserire la condizione per mostrare il risultato di ricerca dopo almeno 3 lettere
-// const showTarget = (keyupEvent) => {
-//   const inputs = document.querySelectorAll(".i_text");
-//   for (let input of inputs) {
-//     if (input.value > 3) {
-//       console.log(input.value);
-//     }
-//     // console.log(keyupEvent.target.value);
-//     else {
-//       console.log("Add letter");
-//     }
-//   }
-// };  + book.value
-// const search = () => {
-//   const searchBook = document.getElementById("searchField");
-//   console.log("book", searchBook);
-//   fetch("https://striveschool-api.herokuapp.com/books?q=" + searchBook.value)
-//     .then((resp) => resp.json())
-//     .then(
-//       (json) => {
-//         console.log(json);
-//         let htmlResults = ``;
-
-//         json.map((book) => {
-//           htmlResults += `
-//             <div class="card" onclick="select(event)">
-//               <img
-//               src="${book.img}"
-//               class="card-img-top"
-//               alt=""
-//               />
-//                 <div class="card-body">
-//                   <h5 class="card-title">${book.title}</h5>
-//                   <h6 class="card-title">${book.category}</h6>
-//                   <div class="flex__button">
-//                     <button onclick=""><ion-icon name="cart-outline"></ion-icon>${book.price.toFixed(
-//                       2
-//                     )}</button>
-//                     <button onclick="hideCard(event)">Nascondi</button>
-//                     <button><a href="./details.html?id=${
-//                       book.asin
-//                     }"> Dettagli</a></button>
-//                   </div>
-//                 </div>
-//             </div>`;
-//           //// <div class="row">
-//           // //     <div class="col-10">
-//           // //       <div id="${book.asin}" class="">
-//           // //         <h2>${bookook.title}</h2>
-//           // //         <div id="${bookook.category}" class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
-//           // //           <img src="${bookook.img}" />
-//           // //         </div>
-//           // //     </div>
-//           // //   </div>
-//           // // </div>`;
-//         });
-//         document.getElementById("containerItem").innerHTML = htmlResults;
-//       }
-//       // Aggiunta dell'HTML
-//       // https://striveschool-api.herokuapp.com/api/deezer/search?q=
-//     );
-// };
+//! SearchBar
